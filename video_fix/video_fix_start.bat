70629b587c855f8fa54b6e4a0200076e0d27a9dc
Anton Potekhin <anton.potekhin@gmail.com>
2015-06-21 12:57:45 +0300

    fix bat file

--- video_fix/video_fix_start.bat
+++ video_fix/video_fix_start.bat
@@ -1,4 +1,4 @@
-adb push ./video_fix.sh /sdcard1/video_fix.sh
+adb push video_fix.sh /sdcard1/video_fix.sh
 adb shell su -c "chmod 755 /sdcard1/video_fix.sh"
 adb shell su -c "sh /sdcard1/video_fix.sh"
 adb shell su -c "rm -f /sdcard1/video_fix.sh"
